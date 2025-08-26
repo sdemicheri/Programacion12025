7fdbd042b1a32e214c99c8621dc2e912109ea01a
YairEB46 <ezequielbustamante74@gmail.com>
2025-08-25 21:39:07 -0300

    agregado problema uno

---
+++ Problema uno.py
@@ -0,0 +1,4 @@
+peso = float(input("Ingrese su altura en metros"))
+altura = float(input("Ingrese su peso en kilogramos"))
+imc = peso / (altura*altura)
+print(f"Tu indice de masa corporal es:{imc:.2f}")
