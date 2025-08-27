e654954f82a06394e67023463fcc76d14b7e52e2
sdemicheri <69224123+sdemicheri@users.noreply.github.com>
2025-08-27 19:42:27 -0300

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,6 +2,7 @@ capital_inicial = float(input("Ingrese la cantidad a invertir "))
 interes_anual = float(input("Ingrese el interés anual en % "))
 años = int(input("Ingrese el número de años: "))
 capital_final = capital_inicial * (1 + interes_anual / 100) ** años
+print("Hola hice lio")
 print("El capital obtenido es:", capital_final)
 
 
