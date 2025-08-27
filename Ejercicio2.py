94212fc0b8583eb3f784453d7f94009969b1ccfa
YairEB46 <ezequielbustamante74@gmail.com>
2025-08-27 19:34:02 -0300

    ejercicio resolucion

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,8 @@
-print("hola mundo")
+capital_inicial = float(input("Ingrese la cantidad a invertir "))
+interes_anual = float(input("Ingrese el interés anual en % "))
+años = int(input("Ingrese el número de años: "))
+capital_final = capital_inicial * (1 + interes_anual / 100) ** años
+print("El capital obtenido es:", capital_final)
 
 
 
