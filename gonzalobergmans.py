697c9cfb2e077d0a122c868796e8e733420d0c65
Gonzalo Bergmans <gonzalobergmans@gmail.com>
2025-08-28 19:37:14 -0300

    proceso

--- gonzalobergmans.py
+++ gonzalobergmans.py
@@ -1,3 +1,20 @@
 #analisis
 #entrada
 #precio del peluche
+#proceso
+# Billetes de $100
+# (237−(2×100)=37) 
+# Billetes de $50
+#37÷50=0 
+# Billetes de $20
+#37÷20=1 
+#37−(1×20)=17.
+# Billetes de $10
+#17÷10=1 billete.
+# 17−(1×10)=7.
+# Billetes de $5
+#7÷5=1
+# 7−(1×5)=2.
+# Billetes de $1
+#2÷1=2 
+#mostrar cuantos billetes de 100, 50, 20,10,5, 1 se ultizaron
