61790e8ec33d499da7fbdc39538b7847cb705108
Tiago <tiagoyisaac@gmail.com>
2025-08-30 10:37:05 -0300

    prueba 1

--- Tiago_liendo.py
+++ Tiago_liendo.py
@@ -1,1 +1,12 @@
-#analisis
+peluche = int(input("ingrese cuanto cuesta el peluche:"))
+ 
+B_cien = 100
+B_cincu = 50
+B_veinte = 20
+B_diez = 10
+B_cinco = 5
+B_uno = 1
+
+R_a = peluche%B_cien
+
+print(R_a)
