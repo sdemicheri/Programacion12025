4217befe421aced0bff277161e582e8392359479
Tiago <tiagoyisaac@gmail.com>
2025-08-30 11:14:23 -0300

    segunda prueba y trabajo finalizado

--- Tiago_liendo.py
+++ Tiago_liendo.py
@@ -7,6 +7,22 @@ B_diez = 10
 B_cinco = 5
 B_uno = 1
 
-R_a = peluche%B_cien
+A_ = peluche%B_cien
+B_ = peluche//B_cien
+C_ = A_%B_cincu
+D_ = A_//B_cincu
+E_ = C_%B_veinte
+F_ = C_//B_veinte
+G_ = E_%B_diez
+H_ = E_//B_diez
+I_ = G_%B_cinco
+J_ = G_//B_cinco
+K_ = I_%B_uno
+L_ = I_//B_uno
 
-print(R_a)
+print("Necesitas:", B_, "billetes de 100")
+print("Necesitas:", D_, "billetes de 50")
+print("Necesitas:", F_, "billetes de 20")
+print("Necesitas:", H_, "billetes de 10")
+print("Necesitas:", J_, "billetes de 5")
+print("Necesitas:", L_, "billetes de 1")
