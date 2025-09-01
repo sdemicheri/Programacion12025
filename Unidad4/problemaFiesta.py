89174bc14d0daa4c2dcf396528272f984177279f
YairEB46 <ezequielbustamante74@gmail.com>
2025-09-01 20:45:17 -0300

    problemaFiesta.py

--- Unidad4/problemaFiesta.py
+++ Unidad4/problemaFiesta.py
@@ -1,12 +1,17 @@
-try:
+try: 
     comida = int(input("Ingese el total de la comida:"))
     bebida_sa = int(input("Ingrese el total de las bebidas sin alcohol"))
     bebida_ca = int(input("Ingrese el total de las bebidas con alcohol"))
     invitados = int(input("Ingrese el total de invitados: "))
     invitados_ca = int(input("Ingrese el total de invitados que beben alcohol: "))
+    importe = comida/invitados + bebida_sa/invitados
+    try: 
+        extra = bebida_ca/invitados_ca
+    except ZeroDivisionError:
+        extra = 0
+        print("No hay invitados que consuman alcohol")
+    else: 
+        print("El importe extra : ", extra)
+    print("El importe que debe abonar cada invitado es de : ", importe)    
 except ValueError:
-    print("Error al ingresar los datos")
-importe = comida/invitados + bebida_sa/invitados
-extra = bebida_ca/invitados_ca
-print("El importe que debe abonar cada invitado es de : ", importe)
-print("El importe extra : ", extra)
+    print("Datos incorrectos")
