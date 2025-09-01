21b33b54473679fcf069e7d5a80bdac5a2c8b343
Tiago <tiagoyisaac@gmail.com>
2025-09-01 20:39:23 -0300

    trabajo 2

--- Unidad4/#ejercicio de prueba.py
+++ Unidad4/#ejercicio de prueba.py
@@ -1,10 +1,14 @@
 #ejercicio de prueba
-comida = int(input("ingrese el total de la comida:; "))
+comida = int(input("ingrese el total de la comida: "))
 bebida_ca = int(input("ingrese el total de la bebida con alcohol: "))
 bebida_sa = int(input("ingrese el total de la bebida sin alcohol: "))
 invitados = int(input("ingrese la cantidad de envitados: "))
 invitados_ca = int(input("ingrese la cantidad de invitados: "))
 importe = comida/invitados+bebida_sa/invitados
-extra =bebida_ca/invitados_ca 
-print("El importe debe abonar cada invitado es de: ", importe)
-print("El import extra por el consumo de alcohol:", extra)
+try: 
+ extra =bebida_ca/invitados_ca
+except ZeroDivisionError:
+ print("No hay personas que consuman alcohol")
+else: 
+ print("El importe debe abonar cada invitado es de: ", importe)
+ print("El import extra por el consumo de alcohol:", extra)
