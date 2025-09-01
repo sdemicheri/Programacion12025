2b1f48ee54ff40ce86df28c8994d28ad919e2065
Gonzalo Bergmans <gonzalobergmans@gmail.com>
2025-09-01 20:27:19 -0300

    ejercicio de invitado

--- Unidad4/ejercicioinvitados.py
+++ Unidad4/ejercicioinvitados.py
@@ -4,6 +4,12 @@ bebida_ca = int(input("ingrese el total de la bebida con alcohol "))
 invitados= int(input("ingrese el total de invitados "))
 invitados_ca = int(input("ingrese el total de invitados que beben alcohol "))
 importe = comida/invitados+bebida_sa/invitados
-extra = bebida_ca/invitados_ca
+try :
+  extra = bebida_ca/invitados_ca
+except  ZeroDivisionError : 
+ extra = 0
+ print("No hay personas que consuman alcohol ")
+else :
+ print("el importe extra por el consumo de alcohol: ", extra)
+
 print("el importe que debes abonar cada invitado es de: ", importe)
-print("el importe extra por el consumo de alcohol: ", extra)
