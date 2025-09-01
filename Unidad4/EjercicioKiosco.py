894a58dd40f939453acc2e44e059ff6ef635e420
YairEB46 <ezequielbustamante74@gmail.com>
2025-09-01 19:17:05 -0300

    código general

--- Unidad4/EjercicioKiosco.py
+++ Unidad4/EjercicioKiosco.py
@@ -3,9 +3,8 @@ Un kiosco vende chocolates a $50 cada uno.
 Si un cliente compra 4 chocolates y además paga un 10% de impuesto sobre el total, 
 calcular cuánto debe pagar.
 """
-chocolates = 4
-precio = 50
+chocolates = int(input("Cantidad de chocolates que compro: "))
+precio = int(input("Ingrese el precio de cada chocolate"))
 impuesto = 0.10
-
 total = chocolates * precio + chocolates * precio * impuesto
 print("Total a pagar:", total)
