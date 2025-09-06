6e1153ee9d028a6bc05f453913c6ee6a08878aed
Celeste <celesteveronese12@gmail.com>
2025-09-06 01:58:02 -0300

    Analisis ayudemos a Lucia

---
+++ Unidad4/analisis Ayudemos a Lucia.py
@@ -0,0 +1,32 @@
+#ENTRADA = precio del peluche, billete100, billete50, billete20, billete10, billete5, billete1
+#PROCESO = pedir precioPeluche, 
+#billete100 = precioPeluche // 100
+#resto = precioPeluche % 100
+#billete50 = resto // 50
+#resto2 = resto % 50
+#billete20 = resto // 20
+#resto3 = resto % 20
+#billete10 = resto // 10
+#resto4 = resto % 10
+#billete5 = resto // 5
+#resto5 = resto % 5
+#billete1= resto
+#si billete100 > 0: 
+#hacer billetes100, "billetes de $100"
+#si no hacer "0 billetes de 100"
+#si billete 50 > 0: 
+#hacer billetes50, "billetes de $50"
+#si no hacer "0 billetes de 50"
+#si billete20 > 0: 
+#hacer billetes20, "billetes de $20"
+#si no hacer "0 billetes de 20"
+#si billete10 > 0: 
+#hacer billetes10, "billetes de $10"
+#si no hacer "0 billetes de 10"
+#si billete5 > 0: 
+#hacer billetes5, "billetes de $5"
+#si no hacer "0 billetes de 5"
+#si billete1 > 0: 
+#hacer billetes1, "billetes de $1"
+#si no hacer "0 billetes de 1"
+#SALIDA = cantidad de billetes de $100, billetes de $ 50, billetes de $ 20, billetes de $ 10, billetes de $ 5, billetes de $ 1
