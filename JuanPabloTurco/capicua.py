c0f027781bc90ada498a74778a6f95f05fef9960
Juan Pablo Turco <turcojuanpablo@gmail.com>
2025-09-08 16:37:02 -0300

    resolucion AC

--- JuanPabloTurco/capicua.py
+++ JuanPabloTurco/capicua.py
@@ -4,10 +4,27 @@
 #Si el número ingresado es negativo, se debe mostrar el mensaje:: "El numero debe ser mayor a 0"
 #Si el número tiene más o menos de tres cifras, se debe mostrar el mensaje: "El numero debe ser de 3 cifras"
 #En caso de que el número cumpla con las condiciones, el programa deberá indicar si el mismo "Es capicua" o "No es capicua"
-try:
-    a=int(input())
-    b=int(input())
-except ValueError:
-    if a==3:
-        print("si")
 
+#Entrada: Numero.
+#Proceso: Pedir numero
+# Comprobar si es positivo o de 3 cifras y mostrar lo oportuno segun el caso
+# Si no hay problema: definir si es capicua
+# Mostrar el resultado.
+#Salida: Confirmacion de numero capicua /o/ pedir un numero positivo /o/ pedir un numero de 3 cifras.
+
+try:
+    num=int(input())
+    if num>=0:
+        if len(str(num))==3:
+            num1=num//100
+            num3=(num%100)%10
+            if num1==num3:
+                print("Es capicua")
+            else:
+                print("No es capicua")
+        else:
+            print("El numero de ser de 3 cifras") #mal ahi profe
+    else:
+        print("El numero debe ser mayor a 0")
+except ValueError:
+    print("Error en el ingreso de los datos")
