e9a0e6e5c09c311850c0da559ba36b90af6a3ff6
YairEB46 <ezequielbustamante74@gmail.com>
2025-09-08 18:23:30 -0300

    Problema de excepciones corregido

--- Unidad4/excepciones.py
+++ Unidad4/excepciones.py
@@ -1,19 +1,9 @@
 #Al probar el código que hice, no me deja dividir el numero 100.
-#try:
-    #numero = int(input("Ingrese un número: "))
-    #if numero % 2 == 0:
-    #    print("El número que ingresó se puede dividir")
-   # else:
-  #      print("El número que ingresó no se puede dividir")
-#except ValueError:
- #   print("El dato que ingresó no es válido para el sistema")
 try:
     numero = int(input("Ingrese un número: "))
-
     if numero % 2 == 0:
-        print("Es divisible")
+        print("El número que ingresó se puede dividir")
     else:
-        print("No es divisible")
-
+        print("El número que ingresó no se puede dividir")
 except ValueError:
-    print("Los datos ingresados no son correctos")
+    print("El dato que ingresó no es válido para el sistema")
