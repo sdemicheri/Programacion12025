1b974550cf15f205597a488ab5603dd462f5bef2
rodo_missan <missanjose18@gmail.com>
2025-09-08 18:46:49 -0300

    Practicamos

---
+++ Unidad4/9_Practicamos.py
@@ -0,0 +1,8 @@
+usuario = input("Ingrese su usuario: ")
+contraseña = input("Ingrese su contraseña: ")
+nombre = "admin"
+contraseña1 = "123456"
+if usuario == nombre and contraseña == contraseña1:
+ print("Acceso concedido")
+else:
+ print("Acceso denegado")
