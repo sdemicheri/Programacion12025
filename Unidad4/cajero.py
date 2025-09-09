5ff8887a8eeb6472ea13d9bd60139539cdbb9aab
YairEB46 <ezequielbustamante74@gmail.com>
2025-09-09 14:03:41 -0300

    Código de cajero

---
+++ Unidad4/cajero.py
@@ -0,0 +1,22 @@
+try:
+    clave = int(input("Ingrese la clave: "))
+    saldo = float(input("Ingrese el saldo de su cuenta "))
+    print("Seleccione 1 para retirar dinero")
+    print("Seleccione 2 para ver el saldo actual de la cuenta")
+    print("Seleccione 3 para salir")
+    opcion = int(input("Seleccione un número: "))
+    if opcion == 1:
+        dinero = float(input("Ingrese la cantidad de dinero a retirar: "))
+        if dinero > saldo:
+            print("No tiene la cantidad suficiente para retirar ese monto")
+        else: saldo >= dinero
+        saldo_final = saldo - dinero
+        print("El dinero se retiró exitosamente. Su saldo actual es: $", saldo_final)
+    elif opcion == 2:
+        print("El saldo actual de la cuenta es: $",saldo)
+    elif opcion == 3:
+     print("Gracias por utilizar nuestro servicio")
+    else:
+        print("Marcó una opción incorrecta")
+except ValueError:
+    print("Ingresó un dato no válido")
