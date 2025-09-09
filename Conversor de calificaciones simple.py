b5225f7a2042c7da401a915c61fd9b717ffbdc4d
Celeste <celesteveronese12@gmail.com>
2025-09-09 17:28:34 -0300

    Conversor de Calificaciones a Numeros - Simple 1

---
+++ Conversor de calificaciones simple.py
@@ -0,0 +1,30 @@
+nota = int(input("Ingrese la calificacón: "))
+match nota:
+    case 1:
+        print("Uno")
+    case 2:
+        print("Dos")
+    case 3:
+        print("tres")
+    case 4:
+        print("Cuatro")
+    case 5:
+        print("Cinco")
+    case 6:
+        print("Seis")
+    case 7:
+        print("Siete")
+    case 8:
+        print("Ocho")
+    case 9:
+        print("Nueve")
+    case 10:
+        print("Diez")
+
+i
+if nota >= 6:
+    print("Aprobado")
+elif nota >= 8:
+    print("Promoción directa")
+elif nota == 10:
+    print("Calificación Perfecta")
