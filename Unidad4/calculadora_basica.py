35e17a1fda43cf79beb740961a881fd4bcd4c562
YairEB46 <ezequielbustamante74@gmail.com>
2025-09-09 15:00:38 -0300

    Código de calculadora corregido

--- Unidad4/calculadora_basica.py
+++ Unidad4/calculadora_basica.py
@@ -1,22 +1,33 @@
-#try:
- #   num1 = float(input("Ingrese el primer numero: "))
-  #  operacion = input("Ingrese la operación a realizar, +,-,/,*,** ")
-   # num2 = float(input("Ingrese el segundo numero: "))
-#if operacion == "+":
- #   resultado = num1 + num2
-  #  print("El resultado es: ", resultado)
-#elif operacion == "-":
- #   resultado = num1 - num2
-  #  print("El resultado es: ", resultado)
-#elif operacion == "/":
- #   if num2 != 0:
-  #   resultado = num1 / num2
-   #  print("El resultado es: ", resultado)
-    #else:
-    #   print("No se puede dividir por 0")
-#elif operacion == "*":
- #  resultado = num1 * num2
-  # print("El resultado es: ", resultado)
-#elif operacion == "**":
- #  resultado = num1 ** num2
-  # print("El resultado es: ", resultado)
+try:
+    num1 = float(input("Ingrese el primer número: "))
+    operacion = input("Ingrese la operación a realizar (+, -, /, *, **): ")
+    num2 = float(input("Ingrese el segundo número: "))
+
+    if operacion == "+":
+        resultado = num1 + num2
+        print("El resultado es: ", resultado)
+
+    elif operacion == "-":
+        resultado = num1 - num2
+        print("El resultado es: ", resultado)
+
+    elif operacion == "/":
+        if num2 != 0:
+            resultado = num1 / num2
+            print("El resultado es: ", resultado)
+        else:
+            print("No se puede dividir por 0")
+
+    elif operacion == "*":
+        resultado = num1 * num2
+        print("El resultado es: ", resultado)
+
+    elif operacion == "**":
+        resultado = num1 ** num2
+        print("El resultado es: ", resultado)
+
+    else:
+        print("Operación no válida")
+
+except ValueError:
+    print("SINTAX ERROR: No ingresó un número")
