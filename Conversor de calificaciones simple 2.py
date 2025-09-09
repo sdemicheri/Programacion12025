9e19910f83ca9fd5bbc0f2e4a5bd7a58bfbefa6d
Celeste <celesteveronese12@gmail.com>
2025-09-09 19:02:45 -0300

    Conversor de calificaciones simple 2

---
+++ Conversor de calificaciones simple 2.py
@@ -0,0 +1,15 @@
+import math
+
+nota = float(input("Ingrese la calificación: "))
+calificacion = math.ceil(nota)
+if calificacion > 0 and calificacion <= 100:
+    if calificacion < 60:
+        print("Reprobado")
+    elif calificacion >= 60 and calificacion < 80:
+        print("Aprobado")
+    elif calificacion >= 80 and calificacion < 100:
+        print("Promocionado")
+    elif calificacion == 90:
+        print("Calificación Perfecta /nFelicitaciones!!")
+else:
+    print("Ingrese los valores correctos.")
