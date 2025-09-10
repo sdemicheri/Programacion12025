b6f3ae681c13dd097d13a290be18e727061dd723
LULY1230 <ludmilaapallaroni12@gmail.com>
2025-09-09 23:55:56 -0300

    Actualizacion

--- Unidad4/Calcular_edad_de_una_persona.py
+++ Unidad4/Calcular_edad_de_una_persona.py
@@ -1,35 +1,57 @@
-#EJEPLO N°1
+
 
 #Solicitar al usuario el año de nacimiento como número entero.
-anio_nacimiento = int(input("Ingre su año de nacimiento: "))
+anio_nacido = int(input("Ingrese su año de nacimiento: "))
+mes_nacido = int(input("Ingrese su mes de nacimiento: "))
+dia_nacido = int(input("Ingrese el día de nacimiento: "))
 
 #Solicitar al usuario el año actual como número entero.
 anio_actual = int(input("Ingrese el año actual: "))
+mes_actual = int(input("Ingrese el mes actual: "))
+dia_actual = int(input("Ingrese el día actual: "))
 
-#Se verifica que el año de nacimiento no sea mayor que el año actual. Si esto ocurre, se muestra un mensaje de error
-if anio_nacimiento < anio_actual:
-    edad = anio_actual - anio_nacimiento
+
+
+if  (anio_actual > anio_nacido): # Calcular la edad de una persona que supera el año de vida
+    if (mes_actual < mes_nacido): # La persona no llego a cumplir la edad
+        edad = (anio_actual - anio_nacido - 1)
+        print("Tu edad es: ", edad, "años" )
+    if (mes_actual > mes_nacido): #La persona ya cumplió la edad
+        edad = (anio_actual - anio_nacido)
+        print("Tu edad es: ", edad, "años" )
+    if (mes_actual == mes_nacido): # Si el mes actual es igual al mes de nacido la edad exacta se calcula con los días
+        if (dia_actual >= dia_nacido):
+            edad = (anio_actual - anio_nacido) # Cumpliria o ya cumplió los años
+            print("Tu edad es: ", edad, "años" )
+        if (dia_actual < dia_nacido): # Esta en el mes donde cumpliria la edad pero todavia no llegó el día
+            edad = (anio_actual - anio_nacido - 1)
+            print("Tu edad es: ", edad, "años" )
+elif (anio_actual == anio_nacido): # calcula la edad de una persona que no supera el año de vida. Ejemplo: un bebé
+    if (mes_actual > mes_nacido): # Se calcula la edad de alguien que tiene unos cuantos meses de vidad
+     edad = (mes_actual - mes_nacido)
+     print("Tu edad es: ", edad, "meses de nacido")
+    elif (mes_actual == mes_nacido): # Se calcula la edad de alguien que todavia no supera el mes de vida
+        if (dia_actual > dia_nacido):
+         edad = (dia_actual - dia_nacido)
+         print("Tu edad es:", edad, "días de nacido")
+        else:
+            print("ERROR. El día actual no puede ser menor al día de nacimiento ")
+    else: 
+        print("ERROR. El mes actual no puede ser menor al mes de nacido")
 else:
-    print("ERROR.El año de nacimiento no puede ser mayor que el año actual")
-
-
-#Calcular la edad
-edad = anio_actual - anio_nacimiento
-
-#Imprimir la edad
-print("La edad de la persona es:", edad)
+    print("ERROR. El año de nacimiento no puede ser mayor al año actual")
 
 
 
-#EJEMPLO N°2
 
-# Solicitar años
-anio_nacimiento = int(input("Año de nacimiento: "))
-anio_actual = int(input("Año actual: "))
 
-# Calcular edad
-edad = anio_actual - anio_nacimiento
+    
+    
 
-# Mostrar resultado
-print("Edad:", edad)  
+    
+    
+
+
+
+
   
