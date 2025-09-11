72e6c345fb21d4e5956d6be085672c7a26272b44
Juan Pablo Turco <turcojuanpablo@gmail.com>
2025-09-10 21:28:43 -0300

    actualizacion cumpleanios

--- JuanPabloTurco/diasParaCumplirAnios.py
+++ JuanPabloTurco/diasParaCumplirAnios.py
@@ -1,21 +1,24 @@
 #Entrada: diaNacimiento - MesNacimiento - DiaActual - MesActual
-#Proceso: Definir meses con sus dias pertinentes, Pedir datos, 
-# restar  
+#Proceso: Pedir datos, ???, mostrar resultado
 #Salida: DiasFaltantesParaCumplirAnios
-print ("Introduzca los datos que se le pide para obtener cuantos dias aproximados faltan para su cumpleanios")
-diaN=int(input("Dia de nacimiento"))
-mesN=int(input("Mes de nacimiento"))
-diaA=int(input("Dia actual"))
-mesA=int(input("Mes actual"))
-i=mesA
-dias=0
-if mesA ==mesN and diaA>=diaN:
-    dias=365-(diaA-diaN)
-else:
-    while i!=mesN:      #Tiene cilclo MAAAAAAAAL
-        dias+=30
-        i+=1
-        if i>12:
-            i=1
-    dias+=30-diaA+(diaN-30)
-print ("Faltan aproximadamente: ", dias, "Dias para tu cumpleanios")
+print ("Introduzca los datos que se le pide en numeros (1, 2, 3, etc)")
+print ("para obtener cuantos dias aproximados faltan para su cumpleanios")
+try:      
+    diaN=int(input("Dia de nacimiento: "))
+    mesN=int(input("Mes de nacimiento: "))
+    diaA=int(input("Dia actual: "))
+    mesA=int(input("Mes actual: "))
+    i=mesA
+    dias=0
+    if mesA ==mesN and diaA>=diaN:
+        dias=365-(diaA-diaN)
+    else:
+        while i!=mesN:      #Tiene ciclo MAAAAAAAAL
+            dias+=30
+            i+=1
+            if i>12:
+                i=1
+        dias+=30-diaA+(diaN-30)
+    print ("Faltan aproximadamente: ", dias, "Dias para tu cumpleanios")
+except ValueError:
+    print("no se haga, sabe lo que hizo")
