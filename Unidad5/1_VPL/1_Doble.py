66f8e5bec754afca3448d69b95ead7f3894a033b
José Missán <missanjose18@gmail.com>
2025-09-13 01:34:48 -0300

    Arreglando error

--- Unidad5/1_VPL/1_Doble.py
+++
@@ -1,8 +0,0 @@
-"""
-Utilizando la estructura repetitiva para. 
-Realice un algoritmo que ingresando 10 números y muestre el doble del mismo.
-"""
-print("Ingrese 10 números")
-for i in range(10):
-    numero = int(input())
-    print(f"{numero*2}")
--- Unidad5/1_VPL/3_TablaMultiplicar.py
+++
@@ -1,8 +0,0 @@
-"""
-Ingrese un número N y muestre la tabla de multiplicar del número. 
-Muestre el multiplicando, el multiplicador y el producto
-N = 7 , 7*1 = 7, 7*2 = 14 ...
-"""
-numero = int(input("Ingrese un número: "))
-for i in range(1,11):
-    print(f"{numero}*{i} = {numero*i}")
