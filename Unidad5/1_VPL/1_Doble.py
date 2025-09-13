67ea919c1fc6e1522075f5805f24225a831a6b6d
José Missán <missanjose18@gmail.com>
2025-09-13 01:25:49 -0300

    Estructura Repetitiva Para (Simple)

---
+++ Unidad5/1_VPL/1_Doble.py
@@ -0,0 +1,8 @@
+"""
+Utilizando la estructura repetitiva para. 
+Realice un algoritmo que ingresando 10 números y muestre el doble del mismo.
+"""
+print("Ingrese 10 números")
+for i in range(10):
+    numero = int(input())
+    print(f"{numero*2}")
