0732b15f42e89360b23b1ba47db40e81ff87e0e2
Juan Pablo Turco <turcojuanpablo@gmail.com>
2025-09-15 17:30:54 -0300

    limpieza de la rama

--- Bergmans/gg.py
+++
@@ -1,5 +0,0 @@
-usuario= input("usuario: ")
-contraseña = input("contraseñera: ")
-if usuario== "admin" and contraseña== "123456" :
-    print("acceso concedido")   
-else:    print("acceso denegado")
