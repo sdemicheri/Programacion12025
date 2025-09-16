67337b76c1dd4176e8a38f09bef318e35f7fd7c7
José Missán <missanjose18@gmail.com>
2025-09-16 03:03:03 -0300

    Subida - Pirámide de Nros

--- Problema uno.py
+++
@@ -1,4 +0,0 @@
-peso = float(input("Ingrese su altura en metros"))
-altura = float(input("Ingrese su peso en kilogramos"))
-imc = peso / (altura*altura)
-print(f"Tu indice de masa corporal es:{imc:.2f}")
---
+++ Unidad5/1_VPL/5_Pirámide.py
@@ -0,0 +1,19 @@
+"""
+Escribir un programa que, dado un número entero positivo N, 
+imprima una pirámide de números invertida donde:
+
+Por ejemplo, si N=5, la salida debería ser:
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+"""
+""""""
+nro = int(input("Ingrese un número entero positivo: ")) 
+while nro > 0:
+    for i in range(1,nro+1): #  itera desde la variable 1 hasta el nro+1
+        print(i,end= ' ')    #  mientras itera con paso 1 imprime esa posicion del nro
+    nro -= 1                 #  al nro le resto 1 para que cada vez se vaya imprimiendo menos cantidad
+    if nro >= 1:             #  por cada impresion de la fila hago un salto de linea,
+        print()              #  excepto cuando solo queda el 1             
