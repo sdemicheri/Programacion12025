4245b42cbc16b720f4ac473731362a351921cf2d
José Missán <missanjose18@gmail.com>
2025-09-20 03:50:21 -0300

    Adivina el nro y Menú casi terminados

--- Unidad5/2_Ejercicios Extras/3_MenuJuegos.py
+++ Unidad5/2_Ejercicios Extras/3_MenuJuegos.py
@@ -1,22 +1,30 @@
-print("-"*20)
+import random
+print("-"*18)
 print("  MENÚ DE JUEGOS  ")
-print("-"*20)
-print("\n")
-print("1. Verdadero o Falso - Argentina")
+print("-"*18)
+print("")
+print("1. Trivia - Argentina")
 print("2. Adivina el Número")
 print("3. Piedra Papel o Tijera - Computadora")
-print("\n")
+print("")
 opcion_elegida = input("Elige un juego (ingresa el número) o '0' para salir: ")
+print("")
 if opcion_elegida == "1":
-    print(f"¡Elegiste Verdadero o Falso - Argentina!")
+    print("-"*46)
+    print("Iniciando: 'Trivia - Argentina' ¡Buena suerte!")
+    print("-"*46)
 elif opcion_elegida == "2":
-    print(f"!Elegiste Adivina el número!")
+    print("-"*45)
+    print("Iniciando: 'Adivina el número' ¡Buena suerte!")
+    print("-"*45)
 elif opcion_elegida == "3":
-    print(f"¡Elegiste Piedra Papel o Tijera!")
+    print("-"*49)
+    print("Iniciando: 'Piedra Papel o Tijera' ¡Buena suerte!")
+    print("-"*49)
 elif opcion_elegida == "0":
-    print(f"Fin del programa")
+    print(f"FINALIZANDO EL PROGRAMA...")
 else:
-    print(f"Opcion no valida, por favor ingrese un numero del 1 al 3 o 0 para finalizar el programa")
+    print(f"Opcion no valida, por favor revise la entrada")
 if opcion_elegida == "3":
     USUARIO = int(input("Eliga una jugada (ingrese el número) 1. Piedra 2. Papel 3. Tijera : "))
     if USUARIO == 1:
@@ -25,7 +33,6 @@ if opcion_elegida == "3":
         jugada1 = "papel"
     elif USUARIO == 3:
         jugada1 = "tijera"
-    import random
     computadora = random.randint(1,3)
     if computadora == 1:
         jugada2 = "piedra"
@@ -46,3 +53,21 @@ if opcion_elegida == "3":
     else:
         print("Los datos ingresados en las jugadas no son validos")
 
+elif opcion_elegida == "2":
+    pc_nro = random.randint(1,10)
+    intentos = 3
+    ganaste = False
+    while intentos > 0 and not ganaste:
+            if intentos == 1:
+                numero = int(input(f"Te queda {intentos} intento, Ingresa tu número del 1 al 10: "))
+            elif intentos == 2 or intentos == 3:
+                numero = int(input(f"Te quedan {intentos} intentos, Ingresa tu número del 1 al 10: "))
+            if pc_nro == numero:
+                if intentos == 3:
+                    print("¡A LA PRIMERA ADIVINASTE!, LOCURAAAA")
+                else:
+                    print("¡ADIVINASTE GENI@, FELICITACIONES!")
+                ganaste = True
+            else:
+                intentos -= 1
+    print("Perdiste pero no pasa nada rey")
