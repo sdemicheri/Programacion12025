7b4e30de756bff1b68cf68a4cf75507f8f3c0d12
Tiago <tiagoyisaac@gmail.com>
2025-09-29 20:36:36 -0300

    funncion 6

---
+++ Funciones/Funcion_6.py
@@ -0,0 +1,8 @@
+
+def dividirGastos():
+ b = int(input("Ingrese el gasto: "))
+ c = int(input("ingrese la cantidad de personas: ")) 
+ a = b / c
+ return a
+
+print("cada persona debe pagar:", dividirGastos())
