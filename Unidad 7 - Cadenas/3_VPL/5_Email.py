78b66789e06fbbf7a5189569d659cec598a966b8
José Missán <missanjose18@gmail.com>
2025-10-07 23:51:34 -0300

    Todos ejercicios resueltos menos el 5_Email.py

--- Unidad 7 - Cadenas/3_VPL/5_Email.py
+++ Unidad 7 - Cadenas/3_VPL/5_Email.py
@@ -1,20 +1,16 @@
 """
 En una empresa de servicios en línea, se necesita un programa que verifique si el correo electrónico 
 ingresado por el usuarios es válido.
-
 Un correo electrónico válido debe contener al menos un carácter antes del símbolo '@', un '@', 
 y al menos un punto ('.') que separa el dominio de nivel superior (TLD, por sus siglas en inglés) 
 del dominio de nivel inferior (SLD, por sus siglas en inglés). 
 Además, se permite que haya un subdominio opcional, que también se separa por un punto.
 """
 correo = input().lower()
-
 contador_arroba = 0
 posicion_arroba = 0
-correo_valido = False
 contador_puntos = 0
 letras_antes_arroba = False
-
 for i in range(0,len(correo)):
     if correo[i] == "@":
         contador_arroba += 1
@@ -26,23 +22,7 @@ for i in range(0,len(correo)):
                 letras_antes_arroba = False
     if correo[i] == ".":
         contador_puntos += 1
-# COMPROBAR CANTIDAD DE ARROBA, (SOLO SE PERMITE UNA)
-if contador_arroba != 1:
-    correo_valido = False
-else:
-    correo_valido = True
-
-if letras_antes_arroba == True:
-    correo_valido = True
-else:
-    correo_valido = False
-# COMPROBAR CANTIDAD DE PUNTOS (AL MENOS DEBE HABER UNO)
-if contador_puntos != 0:
-    correo_valido = True
-else:
-    correo_valido = False
-#COMPROBACION FINAL
-if correo_valido == True:
+if contador_arroba == 1 and letras_antes_arroba == True and contador_puntos != 0 and correo[0] != " ":
     print("mail válido")
-elif correo_valido == False:
+else:
     print("mail inválido")
