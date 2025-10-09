fa2328655de4c866daf027db52835d1d66677773
José Missán <missanjose18@gmail.com>
2025-10-09 02:57:59 -0300

    Palabras Palindromas casi terminado, 3/5 casos pasasados

--- Unidad 7 - Cadenas/3_VPL/12_Palabras palindromas.py
+++ Unidad 7 - Cadenas/3_VPL/12_Palabras palindromas.py
@@ -5,7 +5,7 @@ Un palindromo es una palabra que puede leerse tanto al derecho como al reves.
 
 La busqueda no debe ser sensible a las mayusculas y minusculas.
 """
-cadena = "La visita a neuquen debe ser urgente."                
+cadena = input("Ingrese una oracion: ").lower()                
 palabra = ""
 palabra_invertida = ""
 posicion_espacio = 0
@@ -13,30 +13,33 @@ contar_palindroma = 0
 contar_espacios = 0
 
 for i in range(len(cadena)):
+
     primer_espacio = posicion_espacio
+
     if cadena[i] == " ":
         contar_espacios += 1
         posicion_espacio = i
+
         if contar_espacios == 1:
             for i in range(primer_espacio,posicion_espacio,1):
                 palabra += cadena[i]
             for i in range(posicion_espacio-1,primer_espacio-1,-1):
                 palabra_invertida += cadena[i]
-            if palabra == palabra_invertida:
+            if palabra == palabra_invertida and len(palabra) > 2:
                 contar_palindroma += 1
             else:
                 palabra = ""
                 palabra_invertida = ""
+
         elif contar_espacios > 1:
-            for i in range(primer_espacio,posicion_espacio,1):
+            for i in range(primer_espacio+1,posicion_espacio,1):
                 palabra += cadena[i]
-            for i in range(posicion_espacio-1,primer_espacio-1,-1):
+            for i in range(posicion_espacio-1,primer_espacio,-1):
                 palabra_invertida += cadena[i]
-            if palabra == palabra_invertida:
+            if palabra == palabra_invertida and len(palabra) > 2:
                 contar_palindroma += 1
-            else:
-                palabra = ""
-                palabra_invertida = ""
-      
-print("Cantidad de espacios en la oracion: ",contar_espacios)
+
+        palabra = ""
+        palabra_invertida = ""
+
 print("Cantidad de palabras palindroma: ",contar_palindroma)   
