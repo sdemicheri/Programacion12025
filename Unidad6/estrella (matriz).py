897d22a699b749f783c4f2c29856c16f13f98d79
Celeste Veronese <celesteveronese12@gmail.com>
2025-11-03 20:24:22 -0300

    Ejercicio Estrella

---
+++ Unidad6/estrella (matriz).py
@@ -0,0 +1,20 @@
+lados = 5
+matriz = [[0]*lados for i in range(lados)]
+
+for i in range(lados):
+    for j in range(lados):
+        if lados % 2 != 0:
+            centro = lados // 2
+            matriz[centro][j] = "*"
+            matriz[i][centro] = "*"
+            matriz[i][i] = "*"
+
+paraAtras = lados -1
+for i in range(lados):              
+        matriz[i][paraAtras] = "*"
+        paraAtras -= 1
+
+for i in range(lados):
+    for j in range(lados):
+        print(matriz[i][j],end=" ")
+    print("")
