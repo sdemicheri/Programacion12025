5346ef98984bddb4aab78f70f194d6faedea6a79
Celeste Veronese <celesteveronese12@gmail.com>
2025-11-09 23:22:27 -0300

    AC - Día de Nacimiento

---
+++ Ejercicios de Repaso/AC Día de Nacimiento.py
@@ -0,0 +1,16 @@
+fecha = input()
+if len(fecha) == 10 and fecha[2] == "/" and fecha[5] == "/":
+    dia = fecha[0:2]
+    mes = fecha[3:5]
+    anio = fecha[6:10]
+
+    meses = {"01": "Enero", "02": "Febrero", "03": "Marzo", "04": "Abril","05": "Mayo", "06": "Junio", "07": "Julio", "08": "Agosto","09": "Septiembre", "10": "Octubre", "11": "Noviembre", "12": "Diciembre"}
+    
+    if mes in meses:
+        print("Día:", dia)
+        print("Mes:", meses[mes])
+        print("Año:", anio)
+    else:
+        print("Formato Incorrecto")
+else:
+    print("Formato Incorrecto")
